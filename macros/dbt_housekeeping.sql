01b7290449f5188c0cb6c05725d09736d28ef25c
vaddanki.dbt1@outlook.com <vaddanki.dbt1@outlook.com>
2022-08-14 21:01:51 +0000

    Working upto documentation

--- macros/dbt_housekeeping.sql
+++ macros/dbt_housekeeping.sql
@@ -1,4 +1,4 @@
 {% macro dbt_housekeeping() -%}
-    '{{ invocation_id }}'::string as dbt_batch_id,
+    '{{ invocation_id }}'::varchar(200) as dbt_batch_id,
     '{{ run_started_at }}'::timestamp as dbt_batch_ts
 {%- endmacro %}
